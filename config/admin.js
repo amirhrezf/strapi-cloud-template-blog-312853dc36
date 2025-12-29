52714cdf2f36d41bab72231a2bd6aae77a8e3463
amir <33402334+amird308@users.noreply.github.com>
2025-12-29 23:19:24 +0330

    Add options to auth configuration

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    options: {
+      secure: false,
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
